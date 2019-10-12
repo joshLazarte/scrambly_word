8d36820a0907e06712266d8ff2de7c2c1800032e
Josh <joshualazarte@yahoo.com>
2019-10-12 18:43:09 -0500

    begin level rules

--- classes/Rule.js
+++ classes/Rule.js
@@ -1,25 +1,39 @@
 class Rule {
-    constructor(params) {
-        Object.assign(this, params);
+  constructor(params) {
+    Object.assign(this, params);
+  }
+
+  isInRange(val, range) {
+    return (val - range.min) * (val - range.max) <= 0;
+  }
+
+  wordListIsValid(wordList) {
+    const bigWordSize = this.letterCount - 1;
+    const mediumWordSize = this.letterCount - 2;
+    const numBigWords = wordList.get(bigWordSize).size;
+    const numMediumWords = wordList.get(mediumWordSize).size;
+    let smallWordSize = this.letterCount - 3;
+    let numSmallWords = 0;
+
+    while (smallWordSize >= Math.min(...wordList.keys())) {
+      numSmallWords += wordList.get(smallWordSize).size;
+      smallWordSize--;
     }
 
-    wordListIsValid(wordList) {
-        const bigWordSize = this.letterCount - 1;
-        const mediumWordSize = this.letterCount - 2;
-        const numBigWords = wordList.get(bigWordSize).size;
-        const numMediumWords = wordList.get(mediumWordSize).size;
-        let smallWordSize = this.letterCount - 3;
-        let numSmallWords = 0;
+    return (
+      numBigWords > 0 &&
+      numMediumWords > 0 &&
+      numSmallWords >= this.numWordsToSolve - (numBigWords + numMediumWords)
+    );
+  }
 
-        while (smallWordSize >= Math.min(...wordList.keys())) {
-            numSmallWords += wordList.get(smallWordSize).size;
-            smallWordSize--;
-        }
+  beginner(wordList) {
+    const bigWordSize = this.letterCount - 1;
+    const subwordSizeRange = { min: 2, max: this.letterCount };
+    const answerRange = { min: 3, max: 6 };
 
-        return (numBigWords > 0 &&
-            numMediumWords > 0 &&
-            numSmallWords >= this.numWordsToSolve - (numBigWords + numMediumWords));
-    }
+    return wordList.get(bigWordSize).size > 0;
+  }
 }
 
 module.exports = Rule;
--- temp.js
+++ temp.js
@@ -1,14 +1,19 @@
 //beginner
 // 3 or 4 letter words
-//subwords 2, 3, 4 letter
+//subwords 2 - 4 letter
+//answers 3 - 6
 
 //intermediate
 //4, 5, or 6 letter words
-//subwords 3, 4, 5 or 6 letter
+//subwords 3 - 6 letter
+//answers 4 - 10
 
 //advanced
 //7, or 8 letter words
-//3 - 8
+//subwords 3 - 8 letter
+//answers 6 - 12
 
 //expert
-//8, 9, 10 level words
+//8, 9, 10 letter words
+//subwords 4 - 10 letter
+//answers 6 - 16
