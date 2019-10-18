e834015ca5a77c29d9b90adc9b1af39d05bad45c
Josh Lazarte <joshualazarte@yahoo.com>
2019-10-18 22:04:38 +0000

    JSS scrambled word circle display

--- client/src/components/display/letter/Letter.js
+++ client/src/components/display/letter/Letter.js
@@ -13,19 +13,21 @@ const images = importAll(
   require.context('../../../letter_tiles', false, /\.(png|jpe?g|svg)$/)
 );
 
-const Letter = ({ letter, onClick, id, disabled = true }) => {
+const Letter = ({ letter, onClick, id, disabled = true, transform }) => {
   const handleClick = () => {
     onClick({ letter, id });
   };
 
   return (
+
     <img
       className='Letter'
-      style={{ cursor: disabled ? 'not-allowed' : 'pointer' }}
+      style={{ cursor: disabled ? 'not-allowed' : 'pointer'}}
       alt={letter}
       src={images[`${letter}.png`]}
       onClick={disabled ? null : handleClick}
     />
+
   );
 };
 
--- client/src/components/game/scrambled/Scrambled.js
+++ client/src/components/game/scrambled/Scrambled.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import Letter from '../../display/letter/Letter';
 import Button from '../../display/button/Button';
-import { createUseStyles } from 'react-jss'
-import './Scrambled.scss';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  Scrambled: {
+    position: 'relative',
+    width: props => props.circleSize,
+    height: props => props.circleSize,
+    padding: '0',
+    margin: '5rem auto'
+  }
+});
+
+const getLetterStyles = () => {
+  const letterStyles = {};
+  let angle = 360 / 4;
+  let rot = 0;
+  let offset = 5;
+
+  for (let i = 0; i < 4; i++) {
+    const currentRotation = rot * 1;
+    const currentOffset = currentRotation + offset;
+
+    letterStyles[`letter${i}`] = {
+      display: 'block',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      margin: '-2.5rem',
+      transform: `rotate(-${currentRotation}deg) translate(8rem) rotate(${currentOffset}deg)`
+    };
+
+    offset = -offset;
+    rot = rot + angle;
+  }
+
+  return letterStyles;
+};
 
 const Scrambled = ({ word, guessLetter, currentGuess, scrambleWord }) => {
   const isDisabled = letter => {
@@ -21,10 +56,13 @@ const Scrambled = ({ word, guessLetter, currentGuess, scrambleWord }) => {
     scrambleWord(word);
   };
 
+  const classes = useStyles({ circleSize: '16rem' });
+  const letterStyles = getLetterStyles();
   return (
-    <div className='ScrambledWord'>
-      <div className=''>
-        {word.map(letter => (
+    <div >
+      <div className={classes.Scrambled}>
+        {word.map((letter, i) => (
+        <span style={letterStyles[`letter${i}`]}>
           <Letter
             key={letter.id}
             id={letter.id}
@@ -32,13 +70,14 @@ const Scrambled = ({ word, guessLetter, currentGuess, scrambleWord }) => {
             disabled={isDisabled(letter)}
             onClick={guessLetter}
           />
+          </span>
         ))}
-        <Button
+      </div>
+      <Button
           onClick={handleClick}
           icon={'fas fa-sync'}
           backgroundColor={'#855723'}
         />
-      </div>
     </div>
   );
 };
